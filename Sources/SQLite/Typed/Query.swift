27033b71223a719750d03a00c6e200cdb7bbfc50
Xiu, Shaomin (Mark) <smxiu@telenavsoftware.com>
2025-03-14 18:52:18 +0800

    Fix force try in the FailableIterator.next() and Connection.prepare() methods

--- Sources/SQLite/Typed/Query.swift
+++ Sources/SQLite/Typed/Query.swift
@@ -1009,9 +1009,10 @@ extension Connection {
         let statement = try prepare(expression.template, expression.bindings)
 
         let columnNames = try columnNamesForQuery(query)
-        let rows = try statement.failableNext().map { Row(columnNames, $0) }
 
-        return AnySequence { AnyIterator { rows } }
+        return AnySequence {
+            AnyIterator { statement.next().map { Row(columnNames, $0) } }
+        }
     }
 
     public func prepareRowIterator(_ query: QueryType) throws -> RowIterator {
